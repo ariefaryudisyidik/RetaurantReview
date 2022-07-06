5f651c19ec31d7cf97897eed689db212a6c28ce5
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2022-07-06 07:00:46 +0700

    Initial commit

---
+++ app/src/main/java/id/co/restauranreviewtest/AppModule.kt
@@ -0,0 +1,18 @@
+package id.co.restauranreviewtest
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    @Named("String1")
+    fun provideTestString1() = "This is a string will inject"
+}
